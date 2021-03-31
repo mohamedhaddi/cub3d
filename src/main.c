27c78907715a4f0eb931714baaddeb35a7c4b746
Mohamed Haddi <mhaddi@student.1337.ma>
2021-03-31 14:44:50 +0100

    clean up and format code

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mhaddi <mhaddi@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/28 21:35:54 by mhaddi            #+#    #+#             */
-/*   Updated: 2021/03/30 19:31:33 by mhaddi           ###   ########.fr       */
+/*   Updated: 2021/03/31 14:44:25 by mhaddi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,62 +14,66 @@
 #include "mlx/mlx.h"
 #include <stdlib.h>
 
-int	worldMap[mapWidth][mapHeight] = {
-	{4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4},
-	{4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 4},
-	{4, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4},
-	{4, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4},
-	{4, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 4},
-	{4, 0, 4, 0, 0, 0, 0, 3, 4, 4, 4, 3, 3, 3, 3, 2, 2, 4, 0, 4, 4, 3, 3, 4},
-	{4, 0, 1, 0, 0, 0, 0, 4, 0, 4, 0, 3, 0, 3, 0, 1, 2, 0, 0, 0, 4, 3, 3, 4},
-	{4, 0, 1, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 1, 2, 0, 0, 0, 0, 0, 0, 4},
-	{4, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 3, 3, 4},
-	{4, 0, 2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 2, 0, 0, 0, 0, 0, 0, 4},
-	{4, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 1, 2, 0, 0, 0, 4, 3, 3, 4},
-	{4, 0, 0, 0, 0, 0, 0, 4, 4, 4, 4, 0, 2, 3, 3, 1, 2, 4, 4, 4, 4, 3, 3, 4},
-	{3, 3, 4, 1, 4, 1, 2, 2, 4, 4, 4, 0, 1, 3, 1, 2, 1, 3, 3, 4, 4, 3, 3, 4},
-	{2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4},
-	{1, 4, 4, 4, 4, 2, 0, 4, 4, 4, 3, 0, 3, 3, 1, 2, 1, 3, 4, 4, 3, 3, 1, 4},
-	{4, 3, 4, 4, 4, 4, 0, 4, 4, 4, 3, 0, 3, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 4},
-	{4, 0, 0, 0, 0, 0, 0, 0, 0, 4, 3, 0, 3, 2, 0, 0, 0, 0, 0, 2, 0, 0, 0, 4},
-	{4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 2, 0, 0, 2, 0, 0, 2, 0, 0, 0, 4},
-	{4, 0, 0, 0, 0, 0, 0, 0, 0, 4, 1, 0, 1, 2, 0, 0, 0, 0, 0, 2, 2, 0, 2, 4},
-	{4, 0, 2, 0, 3, 0, 0, 0, 0, 1, 2, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 4},
-	{4, 0, 0, 1, 0, 0, 0, 0, 0, 4, 2, 0, 3, 2, 0, 0, 0, 0, 0, 2, 2, 0, 2, 4},
-	{4, 0, 2, 0, 1, 0, 0, 0, 0, 4, 3, 0, 3, 2, 0, 0, 1, 0, 0, 2, 0, 0, 0, 4},
-	{4, 0, 0, 0, 0, 0, 0, 0, 0, 4, 3, 0, 3, 2, 0, 0, 0, 0, 0, 2, 0, 0, 0, 4},
-	{4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3}};
+int			worldMap[mapWidth][mapHeight] = {
+			{4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4},
+			{4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 4},
+			{4, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4},
+			{4, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4},
+			{4, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 4},
+			{4, 0, 4, 0, 0, 0, 0, 3, 4, 4, 4, 3, 3, 3, 3, 2, 2, 4, 0, 4, 4, 3, 3, 4},
+			{4, 0, 1, 0, 0, 0, 0, 4, 0, 4, 0, 3, 0, 3, 0, 1, 2, 0, 0, 0, 4, 3, 3, 4},
+			{4, 0, 1, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 1, 2, 0, 0, 0, 0, 0, 0, 4},
+			{4, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 3, 3, 4},
+			{4, 0, 2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 2, 0, 0, 0, 0, 0, 0, 4},
+			{4, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 1, 2, 0, 0, 0, 4, 3, 3, 4},
+			{4, 0, 0, 0, 0, 0, 0, 4, 4, 4, 4, 0, 2, 3, 3, 1, 2, 4, 4, 4, 4, 3, 3, 4},
+			{3, 3, 4, 1, 4, 1, 2, 2, 4, 4, 4, 0, 1, 3, 1, 2, 1, 3, 3, 4, 4, 3, 3, 4},
+			{2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4},
+			{1, 4, 4, 4, 4, 2, 0, 4, 4, 4, 3, 0, 3, 3, 1, 2, 1, 3, 4, 4, 3, 3, 1, 4},
+			{4, 3, 4, 4, 4, 4, 0, 4, 4, 4, 3, 0, 3, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 4},
+			{4, 0, 0, 0, 0, 0, 0, 0, 0, 4, 3, 0, 3, 2, 0, 0, 0, 0, 0, 2, 0, 0, 0, 4},
+			{4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 2, 0, 0, 2, 0, 0, 2, 0, 0, 0, 4},
+			{4, 0, 0, 0, 0, 0, 0, 0, 0, 4, 1, 0, 1, 2, 0, 0, 0, 0, 0, 2, 2, 0, 2, 4},
+			{4, 0, 2, 0, 3, 0, 0, 0, 0, 1, 2, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 4},
+			{4, 0, 0, 1, 0, 0, 0, 0, 0, 4, 2, 0, 3, 2, 0, 0, 0, 0, 0, 2, 2, 0, 2, 4},
+			{4, 0, 2, 0, 1, 0, 0, 0, 0, 4, 3, 0, 3, 2, 0, 0, 1, 0, 0, 2, 0, 0, 0, 4},
+			{4, 0, 0, 0, 0, 0, 0, 0, 0, 4, 3, 0, 3, 2, 0, 0, 0, 0, 0, 2, 0, 0, 0, 4},
+			{4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3}};
 
 /**
  * test
  */
 
-t_texture loadImage(char *path, t_data *params)
+t_texture	loadImage(char *path, t_data *params)
 {
 	t_texture	texture_buffer;
-	t_mlx 		*mlx;
+	t_mlx		*mlx;
 
 	mlx = &params->mlx;
 
-	texture_buffer.texture_img_data.img = mlx_xpm_file_to_image(mlx->ptr, path, &texture_buffer.width, &texture_buffer.height);
+	texture_buffer.texture_img_data.img = mlx_xpm_file_to_image(
+		mlx->ptr, path, &texture_buffer.width, &texture_buffer.height);
 
-	if (texture_buffer.texture_img_data.img == NULL || texture_buffer.width != texSize || texture_buffer.height != texSize)
+	if (texture_buffer.texture_img_data.img == NULL ||
+		texture_buffer.width != texSize || texture_buffer.height != texSize)
 	{
-		//ft_putstr_fd("Error\n Invalid texture", 2);
+		// ft_putstr_fd("Error\n Invalid texture", 2);
 		printf("Error: Invalid texture\n");
 		printf("texWidth: %d\n", texture_buffer.width);
 		printf("texHeight: %d\n", texture_buffer.height);
 		exit(0);
 	}
 
-	texture_buffer.texture_img_data.addr = (int *)mlx_get_data_addr(texture_buffer.texture_img_data.img,
-												&texture_buffer.texture_img_data.bits_per_pixel,
-												&texture_buffer.texture_img_data.line_length,
-												&texture_buffer.texture_img_data.endian);
+	texture_buffer.texture_img_data.addr =
+		(int *)mlx_get_data_addr(texture_buffer.texture_img_data.img,
+									&texture_buffer.texture_img_data.bits_per_pixel,
+									&texture_buffer.texture_img_data.line_length,
+									&texture_buffer.texture_img_data.endian);
+
 	return (texture_buffer);
 }
 
-int	draw_frame(t_data *params)
+int			draw_frame(t_data *params)
 {
 	t_mlx		*mlx;
 	t_img_data	*img;
@@ -97,10 +101,14 @@ int	draw_frame(t_data *params)
 	char		*outputPosY;
 	char		*outputDirX;
 	char		*outputDirY;
-    int         texNum;
-    double      wallX; 
+	int			texNum;
+	double		wallX;
 	int			ceilingColor;
 	int			floorColor;
+	char		*outputKeystrokesF;
+	char		*outputKeystrokesB;
+	char		*outputKeystrokesR;
+	char		*outputKeystrokesL;
 
 	mlx = &params->mlx;
 	img = &params->img;
@@ -126,7 +134,8 @@ int	draw_frame(t_data *params)
 	for (int x = 0; x < screenWidth; x++)
 	{
 		// calculate ray position and direction
-		double cameraX = 2 * x / (double)screenWidth - 1; // x-coordinate in camera space
+		double cameraX =
+			2 * x / (double)screenWidth - 1; // x-coordinate in camera space
 		rayDirX = player->dirX + player->planeX * cameraX;
 		rayDirY = player->dirY + player->planeY * cameraX;
 
@@ -205,7 +214,9 @@ int	draw_frame(t_data *params)
 			drawEnd = screenHeight - 1;
 
 		// texturing calculations
-		texNum = worldMap[mapX][mapY] - 1; // 1 subtracted from it so that texture 0 can be used!
+		texNum =
+			worldMap[mapX][mapY] - 1; 	// 1 subtracted from it so that
+										// texture 0 can be used!
 
 		// calculate value of wallX (where exactly the wall was hit)
 		if (side == 0)
@@ -215,7 +226,7 @@ int	draw_frame(t_data *params)
 		wallX -= floor(wallX);
 
 		// x coordinate on the texture
-        // texSize and texSize are width and height in texels of the textures
+		// texSize and texSize are width and height in texels of the textures
 		texX = (int)(wallX * (double)texSize);
 		if (side == 0 && rayDirX > 0)
 			texX = texSize - texX - 1;
@@ -226,21 +237,26 @@ int	draw_frame(t_data *params)
 		step = 1.0 * texSize / lineHeight;
 
 		// Starting texture coordinate
-		double texPos = (drawStart - screenHeight / 2.0 + lineHeight / 2.0) * step;
+		double texPos =
+			(drawStart - screenHeight / 2.0 + lineHeight / 2.0) * step;
 
-		for (int y = drawStart; y < drawEnd; y++)
+		for (int y = drawStart; y <= drawEnd; y++)
 		{
 			// Cast the texture coordinate to integer, and mask with (texSize - 1) in case of overflow
 			texY = (int)texPos & (texSize - 1);
+
 			texPos += step;
 
-			color = world->textures[texNum].texture_img_data.addr[texSize * texY + texX];
+			color =
+				world->textures[texNum].texture_img_data.addr[texSize * texY + texX];
 
-			// make color darker for y-sides: R, G and B byte each divided through two with a "shift" and an "and"
+			// make color darker for y-sides: R, G and B byte each divided
+			// through two with a "shift" and an "and"
 			if (side == 1)
 				color = (color >> 1) & 8355711;
 
-			world->buffer[y][x] = color; // y-coordinate first because it works per scanline
+			world->buffer[y][x] =
+				color; // y-coordinate first because it works per scanline
 		}
 	}
 
@@ -250,16 +266,15 @@ int	draw_frame(t_data *params)
 
 	mlx_put_image_to_window(mlx->ptr, mlx->win, img->img, 0, 0);
 
-    // show real-time values for cetain variables on screen
+	// show real-time values for cetain variables on screen
 	outputPosX = (char *)malloc(20 * sizeof(char));
 	outputPosY = (char *)malloc(20 * sizeof(char));
 	outputDirX = (char *)malloc(20 * sizeof(char));
 	outputDirY = (char *)malloc(20 * sizeof(char));
-    char *outputKeystrokesF = (char *)malloc(20 * sizeof(char));
-    char *outputKeystrokesB = (char *)malloc(20 * sizeof(char));
-    char *outputKeystrokesR = (char *)malloc(20 * sizeof(char));
-    char *outputKeystrokesL = (char *)malloc(20 * sizeof(char));
-
+	outputKeystrokesF = (char *)malloc(20 * sizeof(char));
+	outputKeystrokesB = (char *)malloc(20 * sizeof(char));
+	outputKeystrokesR = (char *)malloc(20 * sizeof(char));
+	outputKeystrokesL = (char *)malloc(20 * sizeof(char));
 	sprintf(outputPosX, "posX: %f", player->posX);
 	sprintf(outputPosY, "posY: %f", player->posY);
 	sprintf(outputDirX, "dirX: %f", player->dirX);
@@ -268,7 +283,6 @@ int	draw_frame(t_data *params)
 	sprintf(outputKeystrokesB, "keyB: %d", params->keystrokes[125]);
 	sprintf(outputKeystrokesR, "keyR: %d", params->keystrokes[124]);
 	sprintf(outputKeystrokesL, "keyL: %d", params->keystrokes[123]);
-
 	mlx_string_put(mlx->ptr, mlx->win, 0, 0, 0x00FFFFFF, outputPosX);
 	mlx_string_put(mlx->ptr, mlx->win, 0, 20, 0x00FFFFFF, outputPosY);
 	mlx_string_put(mlx->ptr, mlx->win, 0, 40, 0x00FFFFFF, outputDirX);
@@ -281,7 +295,7 @@ int	draw_frame(t_data *params)
 	return (0);
 }
 
-int	read_keys(t_data *params)
+int			read_keys(t_data *params)
 {
 	t_player	*player;
 	double		oldDirX;
@@ -292,19 +306,23 @@ int	read_keys(t_data *params)
 	// move forward if no wall in front of you
 	if (params->keystrokes[126])
 	{
-		if (worldMap[(int)(player->posX + player->dirX * player->moveSpeed)][(int)(player->posY)] == 0 /*0 enum*/)
+		if (worldMap[(int)(player->posX + player->dirX * player->moveSpeed)]
+					[(int)(player->posY)] == 0 /*0 enum*/)
 			// collision detection: won't move if it ain't 0 (a wall)
 			player->posX += player->dirX * player->moveSpeed;
-		if (worldMap[(int)(player->posX)] [(int)(player->posY + player->dirY * player->moveSpeed)] == 0)
+		if (worldMap[(int)(player->posX)]
+					[(int)(player->posY + player->dirY * player->moveSpeed)] == 0)
 			player->posY += player->dirY * player->moveSpeed;
 	}
 
 	// move backwards if no wall behind you
 	if (params->keystrokes[125])
 	{
-		if (worldMap[(int)(player->posX - player->dirX * player->moveSpeed)][(int)(player->posY)] == 0)
+		if (worldMap[(int)(player->posX - player->dirX * player->moveSpeed)]
+					[(int)(player->posY)] == 0)
 			player->posX -= player->dirX * player->moveSpeed;
-		if (worldMap[(int)(player->posX)][(int)(player->posY - player->dirY * player->moveSpeed)] == 0)
+		if (worldMap[(int)(player->posX)]
+					[(int)(player->posY - player->dirY * player->moveSpeed)] == 0)
 			player->posY -= player->dirY * player->moveSpeed;
 	}
 
@@ -313,12 +331,15 @@ int	read_keys(t_data *params)
 	{
 		// both camera direction and camera plane must be rotated
 		oldDirX = player->dirX;
-		player->dirX = player->dirX * cos(-player->rotSpeed) - player->dirY * sin(-player->rotSpeed);
-		player->dirY = oldDirX * sin(-player->rotSpeed) + player->dirY * cos(-player->rotSpeed);
-
+		player->dirX = player->dirX * cos(-player->rotSpeed) -
+			player->dirY * sin(-player->rotSpeed);
+		player->dirY = oldDirX * sin(-player->rotSpeed) +
+			player->dirY * cos(-player->rotSpeed);
 		oldPlaneX = player->planeX;
-		player->planeX = player->planeX * cos(-player->rotSpeed) - player->planeY * sin(-player->rotSpeed);
-		player->planeY = oldPlaneX * sin(-player->rotSpeed) + player->planeY * cos(-player->rotSpeed);
+		player->planeX = player->planeX * cos(-player->rotSpeed) -
+			player->planeY * sin(-player->rotSpeed);
+		player->planeY = oldPlaneX * sin(-player->rotSpeed) +
+			player->planeY * cos(-player->rotSpeed);
 	}
 
 	// rotate to the left
@@ -326,20 +347,22 @@ int	read_keys(t_data *params)
 	{
 		// both camera direction and camera plane must be rotated
 		oldDirX = player->dirX;
-		player->dirX = player->dirX * cos(player->rotSpeed) - player->dirY * sin(player->rotSpeed);
-		player->dirY = oldDirX * sin(player->rotSpeed) + player->dirY * cos(player->rotSpeed);
-
+		player->dirX = player->dirX * cos(player->rotSpeed) -
+			player->dirY * sin(player->rotSpeed);
+		player->dirY = oldDirX * sin(player->rotSpeed) +
+			player->dirY * cos(player->rotSpeed);
 		oldPlaneX = player->planeX;
-		player->planeX = player->planeX * cos(player->rotSpeed) - player->planeY * sin(player->rotSpeed);
-		player->planeY = oldPlaneX * sin(player->rotSpeed) + player->planeY * cos(player->rotSpeed);
+		player->planeX = player->planeX * cos(player->rotSpeed) -
+			player->planeY * sin(player->rotSpeed);
+		player->planeY = oldPlaneX * sin(player->rotSpeed) +
+			player->planeY * cos(player->rotSpeed);
 	}
 
 	draw_frame(params);
-
 	return (0);
 }
 
-int	key_press(int keycode, t_data *params)
+int			key_press(int keycode, t_data *params)
 {
 	if (keycode == 123 || keycode == 124 || keycode == 125 || keycode == 126)
 	{
@@ -348,7 +371,7 @@ int	key_press(int keycode, t_data *params)
 	return (0);
 }
 
-int	key_release(int keycode, t_data *params)
+int			key_release(int keycode, t_data *params)
 {
 	if (keycode == 123 || keycode == 124 || keycode == 125 || keycode == 126)
 	{
@@ -357,28 +380,29 @@ int	key_release(int keycode, t_data *params)
 	return (0);
 }
 
-int	main()
+int			main()
 {
 	t_data		params;
 	t_mlx		*mlx;
 	t_img_data	*img;
 	t_player	*player;
 	t_world		*world;
-    int         *keystrokes;
+	int			*keystrokes;
 
 	mlx = &params.mlx;
 	img = &params.img;
 	player = &params.player;
 	world = &params.world;
 
-    // initiliaze keystrokes values with 0
-    keystrokes = &params.keystrokes[0];
-    for (int i = 123; i < 127; i++)
-      keystrokes[i] = 0;
+	// initiliaze keystrokes values with 0
+	keystrokes = &params.keystrokes[0];
+	for (int i = 123; i < 127; i++)
+		keystrokes[i] = 0;
 
 	player->posX = 22, player->posY = 12; // x and y start position of the player
 	player->dirX = -1, player->dirY = 0;  // initial direction vector
-	player->planeX = 0, player->planeY = 0.66; // the 2d raycaster version of camera plane
+	player->planeX = 0,
+	player->planeY = 0.66; // the 2d raycaster version of camera plane
 	player->moveSpeed = 0.07;
 	player->rotSpeed = 0.03;
 
@@ -387,12 +411,13 @@ int	main()
 
 	// generate some textures
 	world->textures[0] = loadImage("../assets/textures/gold.xpm", &params);
-  	world->textures[1] = loadImage("../assets/textures/grass.xpm", &params);
-  	world->textures[2] = loadImage("../assets/textures/water.xpm", &params);
-  	world->textures[3] = loadImage("../assets/textures/iron.xpm", &params);
+	world->textures[1] = loadImage("../assets/textures/grass.xpm", &params);
+	world->textures[2] = loadImage("../assets/textures/water.xpm", &params);
+	world->textures[3] = loadImage("../assets/textures/iron.xpm", &params);
 
 	img->img = mlx_new_image(mlx->ptr, screenWidth, screenHeight);
-	img->addr = (int *)mlx_get_data_addr(img->img, &img->bits_per_pixel, &img->line_length, &img->endian);
+	img->addr = (int *)mlx_get_data_addr(
+		img->img, &img->bits_per_pixel, &img->line_length, &img->endian);
 
 	// this was the game loop that draws a whole frame and reads the input every time
 	draw_frame(&params);
