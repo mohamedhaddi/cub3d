72d8079a3bb3dd7b7b30d9b6df5f9cee303276fd
Mohamed Haddi <mhaddi@student.1337.ma>
2021-05-28 16:26:33 +0100

    show wall textures depending on which side the player's facing

--- src/init/game_loading/set_texture_paths.c
+++ src/init/game_loading/set_texture_paths.c
@@ -6,7 +6,7 @@
 /*   By: mhaddi <mhaddi@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/19 17:42:20 by mhaddi            #+#    #+#             */
-/*   Updated: 2021/05/22 18:50:38 by mhaddi           ###   ########.fr       */
+/*   Updated: 2021/05/28 16:19:33 by mhaddi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 void	set_texture_paths(t_world *world, t_config *config)
 {
-	world->texture_paths.walls_facing_north = config->tex.so;
-	world->texture_paths.walls_facing_south = config->tex.no;
-	world->texture_paths.walls_facing_east = config->tex.we;
-	world->texture_paths.walls_facing_west = config->tex.ea;
+	world->texture_paths.walls_facing_north = config->tex.no;
+	world->texture_paths.walls_facing_south = config->tex.so;
+	world->texture_paths.walls_facing_east = config->tex.ea;
+	world->texture_paths.walls_facing_west = config->tex.we;
 	world->texture_paths.sprite = config->tex.sp;
 }
